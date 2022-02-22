e51e197fc8fa37b7100d3498a66afc71a4fa151e
Aion Chiu <aion.chiu@isobar.com>
2022-02-22 14:53:35 +0800

    feat: 切換

--- src/components/game/index.jsx
+++ src/components/game/index.jsx
@@ -1,4 +1,4 @@
-import React, {memo} from 'react';
+import React, { memo, useState } from 'react';
 import PropTypes from 'prop-types';
 import Pentagon from '@/assets/bg-pentagon.svg?component';
 import { MEMBERS } from '@/logic';
@@ -8,41 +8,106 @@ const GameBackGround = () => (
     <Pentagon></Pentagon>
 );
 
-function Shape({id, name, className}) {
+function Shape({ id, name, className, onClick, size }) {
     return (
-        <div className={clsx(
+        <div
+            style={{
+                width: `${size || 120}px`,
+                height: `${size || 120}px`
+            }}
+            className={clsx(
             className,
-            'absolute rounded-full',
-            'w-[120px] h-[120px] z-1',
-            'flex justify-center items-center',
+            'rounded-full cursor-pointer',
+            'flex justify-center items-center z-1',
             `shape-color-${name}`
         )}
-            // onclick={}
+            onClick={() => onClick(id)}
         >
-            <div className={clsx(
-                'w-[90px] h-[90px]',
+            <div
+                style={{
+                    width: `${(parseInt(size, 10)) - (size / 4) || 90}px`,
+                    height: `${(parseInt(size, 10)) - (size / 4) || 90}px`
+                }}
+                className={clsx(
                 'rounded-full bg-white',
                 'flex justify-center items-center'
             )}>
-                <div className={'icon-' + name}></div>
+                <div
+                    style={{
+                        width: `${(parseInt(size, 10)) / 2}px`,
+                        height: `${(parseInt(size, 10)) / 2}px`
+                    }}
+                    className={'icon-' + name}
+                ></div>
             </div>
         </div>
     );
 }
 
 export function GameBase() {
+
+    const [choiceShape, setChoiceShape] = useState(null);
+
+    const player = MEMBERS.find(i => i.id === choiceShape);
+
     return (
         <>
-            <div className="grid grid-cols-[0.4fr] place-content-center mt-[100px]">
-                <div className="relative">
-                    <GameBackGround />
-                    <Shape className="top-0 left-1/2 -translate-x-1/2 -translate-y-1/2" {...MEMBERS[0]}/>
-                    <Shape className="top-[25%] right-0 translate-x-1/2" {...MEMBERS[1]}/>
-                    <Shape className="top-[100%] right-[7%] -translate-y-1/2" {...MEMBERS[2]}/>
-                    <Shape className="top-[100%] left-[7%] -translate-y-1/2" {...MEMBERS[3]}/>
-                    <Shape className="top-[25%] left-0 -translate-x-1/2" {...MEMBERS[4]}/>
+            {choiceShape == null && (
+                <div className="grid grid-cols-[0.4fr] place-content-center mt-[100px]">
+                    <div className="relative">
+                        <GameBackGround />
+                        <Shape
+                            className="absolute top-0 left-1/2 -translate-x-1/2 -translate-y-1/2"
+                            onClick={setChoiceShape}
+                            {...MEMBERS[0]}
+                        />
+                        <Shape
+                            className="absolute top-[25%] right-0 translate-x-1/2"
+                            onClick={setChoiceShape}
+                            {...MEMBERS[1]}
+                        />
+                        <Shape
+                            className="absolute top-[100%] right-[7%] -translate-y-1/2"
+                            onClick={setChoiceShape}
+                            {...MEMBERS[2]}
+                        />
+                        <Shape
+                            className="absolute top-[100%] left-[7%] -translate-y-1/2"
+                            onClick={setChoiceShape}
+                            {...MEMBERS[3]}
+                        />
+                        <Shape
+                            className="absolute top-[25%] left-0 -translate-x-1/2"
+                            onClick={setChoiceShape}
+                            {...MEMBERS[4]}
+                        />
+                    </div>
                 </div>
-            </div>
+            )}
+            {player && (
+                <div className="grid grid-cols-2 place-content-center mt-[100px]">
+                    <div className="flex flex-col justify-center relative">
+                        <h2 className="text-center text-white text-[36px] tracking-widest">YOU PICKED</h2>
+                        <div className="mt-[100px]">
+                            <Shape
+                                size="300"
+                                className=""
+                                {...player}
+                            />
+                        </div>
+                    </div>
+                    <div className="flex flex-col justify-center relative">
+                        <h2 className="text-center text-white text-[36px] tracking-widest">THE HOUSE PICKED</h2>
+                        <div className="mt-[100px]">
+                            {/* <Shape
+                                size="300"
+                                className=""
+                                {...player}
+                            /> */}
+                        </div>
+                    </div>
+                </div>
+            )}
         </>
     );
 }
@@ -52,5 +117,7 @@ export const Game = memo(GameBase);
 Shape.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
-    className: PropTypes.string
+    className: PropTypes.string,
+    onClick: PropTypes.func,
+    size: PropTypes.string
 };
